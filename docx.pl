9b2d1ead45751e5fa7aefec10e83601707c95b5f
Sornram9254 <sornram9254@yahoo.co.th>
2022-09-02 19:54:42 +0700

    Update docx.pl

--- docx.pl
+++ docx.pl
@@ -73,3 +73,10 @@ $zip->writeToFileNamed($path_to_docx_file_new) == AZ_OK or die "Failed to save z
 sleep(3);
 rmtree( $path_to_unzip ) or die "\nCouldn't remove $dir directory, $!";
 print("\t[x] Docx file written");
+
+
+=pod
+# TODO
+my $regex_hyperlinkand_no = qr/<w:t xml:space="preserve">(\d+)<\/w:t><\/w:r><\/w:p><\/w:tc><w:tc><w:tcPr><w:tcW w:w="(\d+)" w:type="pct"\/><\/w:tcPr><w:p w14:paraId="(\w{8})" w14:textId="(\w{8})" w:rsidR="(\w{8})" w:rsidRPr="(\w{8})" w:rsidRDefault="(\w{8})" w:rsidP="(\w{8})"><w:pPr><w:rPr><w:rFonts w:ascii="(.*?)" w:hAnsi="(.*?)"\/><w:b w:val="(\d+)"\/><w:bCs\/><w:sz w:val="(\d+)"\/><w:szCs w:val="(\d+)"\/><\/w:rPr><\/w:pPr><w:proofErr w:type="gramStart"\/><w:r w:rsidRPr="(\w{8})"><w:rPr><w:rFonts w:ascii="(.*?)" w:hAnsi="(.*?)"\/><w:b w:val="(\d+)"\/><w:sz w:val="(\d+)"\/><w:szCs w:val="(\d+)"\/><\/w:rPr><w:t xml:space="preserve">(.*?)<\/w:t>/mp;
+my $replace_finding_no = "<w:t xml:space=\"preserve\">$index<\/w:t><\/w:r><\/w:p><\/w:tc><w:tc><w:tcPr><w:tcW w:w=\"$2\" w:type=\"pct\"\/><\/w:tcPr><w:p w14:paraId=\"$3\" w14:textId=\"$4\" w:rsidR=\"$5\" w:rsidRPr=\"$6\" w:rsidRDefault=\"$7\" w:rsidP=\"$8\"><w:pPr><w:rPr><w:rFonts w:ascii=\"$9\" w:hAnsi=\"$10\"\/><w:b w:val=\"$11\"\/><w:bCs\/><w:sz w:val=\"$12\"\/><w:szCs w:val=\"$13\"\/><\/w:rPr><\/w:pPr><w:proofErr w:type=\"gramStart\"\/><w:r w:rsidRPr=\"$14\"><w:rPr><w:rFonts w:ascii=\"$15\" w:hAnsi=\"$16\"\/><w:b w:val=\"$17\"\/><w:sz w:val=\"$18\"\/><w:szCs w:val=\"$19\"\/><\/w:rPr><w:t xml:space=\"preserve\">$20<\/w:t>";
+=cut
